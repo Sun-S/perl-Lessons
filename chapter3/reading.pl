d5fd6fd5e5c04a5d42055f583366a21a23259e9e
Sun-S <linux@linux-ElementaryOS.(none)>
2016-11-24 11:27:48 +0800

    create a programme.

--- chapter3/reading.pl
+++ chapter3/reading.pl
@@ -3,5 +3,8 @@ use warnings;
 use strict;
 use diagnostics;
 use utf8;
-open my $PROTEIN, '<', $protein or die "$0 :failed to open input file ' $protein' : $!\n";
-
+my $protein = "protein.pep";
+open my $PROTEIN, '<', $protein or die "$0 : failed to open input file ' $protein ' : $!\n";
+$protein = <$PROTEIN>;
+close $PROTEIN or warn "$0 : failed to close input file '$protein' : $!\n";
+print $protein;
