834e9510dc80d6654595211d67525d3d3e1264cb
linux <linux@linux-ElementaryOS.(none)>
2016-12-01 10:22:36 +0800

    add

---
+++ chapter5/if.pl
@@ -0,0 +1,17 @@
+#!/bin/usr/perl -w
+use strict;
+use utf8;
+print "please input something\n";
+
+my $word = <STDIN>;
+
+if ( $word eq 'HIKNHH') {
+     print "HIKNHH\n";
+}
+elsif ( $word eq 'NNNJJJKKK' ) {
+    print "no\n";
+}
+else { 
+  print "wrong!\n";
+}
+exit;
