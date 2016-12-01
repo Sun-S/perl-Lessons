9cad27fe384ab96958445b16b4b244c0a851b38b
linux <linux@linux-ElementaryOS.(none)>
2016-12-01 10:37:36 +0800

    complete half

---
+++ chapter5/writing.pl
@@ -0,0 +1,11 @@
+#!/bin/usr/perl -w
+use strict;
+use utf8;
+my $protein = 'protein.pep';
+open my $PROTEIN, '>', $protein or die "$0: failed to write $protein : $!\n";
+while ( $protein = <$PROTEIN> ) {
+  print " here is the line :\n";
+  print PROTEIN;
+}
+close PROTEIN or warn "$0: failed to close\n";
+exit;
